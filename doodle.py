f3c9a98748d68efd03017b81212551ecffc038c9
parksuho <tngh860@naver.com>
2025-07-19 22:32:14 +0900

    add some info to README.md

--- doodle.py
+++ doodle.py
@@ -1,3 +1,6 @@
+import site
+import time
+from selenium.common.exceptions import SessionNotCreatedException
 
 # K-hentai . org 의 북마크 익스포트 형식
 # bookmark = [{"name":"ㄷㄱㅈㄷㄱ","private":1,"created":1752910021964,"updated":1752910046812,"galleries":[{"id":3446484,"time":1752910035865},{"id":3446480,"time":1752910046812}]}]
@@ -16,3 +19,25 @@
 # print(",".join(list_1))
 
 
+# <script>
+# const favorites = [{"id":27549,"name":"\u3137\u3131\u3148\u3137\u3131","private":1,"gallery_count":4,"created":1752910021964,"updated":1752918669727,"galleries":[{"id":3446484,"time":1752910035865},{"id":3446480,"time":1752910046812},{"id":3446856,"time":1752916219516},{"id":3334085,"time":1752918669727}]}];
+# const favoriteThumbnails = [{"id":27549,"gallery_id":3334085,"gallery_thumb_extension":"webp"}];
+# </script>
+
+# favorites = [{"id":27549,"name":"\u3137\u3131\u3148\u3137\u3131","private":1,"gallery_count":4,"created":1752910021964,"updated":1752918669727,"galleries":[{"id":3446484,"time":1752910035865},{"id":3446480,"time":1752910046812},{"id":3446856,"time":1752916219516},{"id":3334085,"time":1752918669727}]}]
+# print(favorites[0]["id"])
+# print(favorites[0]["galleries"][0]["id"])
+
+# https://k-hentai.org/?search=fav%3A27549
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://k-hentai.org/?search=fav%3A27549")
+    # page.fill("[name=username]", "YOUR_ID")
+    # page.fill("[name=password]", "YOUR_PW")
+    # page.click("button[type=submit]")
+    page.wait_for_function()
+    # favorites_html = page.content()
+    browser.close()
--- main.py
+++ main.py
@@ -6,104 +6,40 @@ import requests
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import time
 
-url = "3446017"
 
-if url.isdigit():
-    url = "https://k-hentai.org/r/" + str(url)
 
-else:
-    pass
 
-gallery_id = url.split("/")[-1]
+#https://k-hentai.org/?search=fav%3A27549
 
-def fetch_gallery_info_http(def_gallery_id):
-    def_url = f"https://k-hentai.org/r/{def_gallery_id}"
-    html = requests.get(def_url, headers={"User-Agent": "Mozilla/5.0"}, timeout=5).text
+# fav_user_id = '27549'
+#
+# def fetch_gallery_info_http(def_fav_user_id):
+#     def_url = f"https://k-hentai.org/?search=fav%3Aasd"
+#
+#     print(def_url)
+#
+#     html = requests.get(def_url, headers={"User-Agent": "Mozilla/5.0"}, timeout=5).text
+#
+#
+#     print(html)
+#
+#     # def_m = re.search(r'const gallery\s*=\s*({.*?});', html, re.DOTALL)
+#     # if not def_m:
+#     #     raise RuntimeError("CANT NOT FIND GALLERY INFO")
+#     # obj = def_m.group(1)
+#     #
+#     # try:
+#     #     return json.loads(obj)
+#     # except json.JSONDecodeError:
+#     #     fixed = re.sub(r'(\w+):', r'"\1":', obj).replace("'", '"')
+#     #     return json.loads(fixed)
+#
+#
+#
+# info = fetch_gallery_info_http(fav_user_id)
 
-    def_m = re.search(r'const gallery\s*=\s*({.*?});', html, re.DOTALL)
-    if not def_m:
-        raise RuntimeError("CANT NOT FIND GALLERY INFO")
-    obj = def_m.group(1)
 
-    try:
-        return json.loads(obj)
-    except json.JSONDecodeError:
-        fixed = re.sub(r'(\w+):', r'"\1":', obj).replace("'", '"')
-        return json.loads(fixed)
 
-def get_tags():
-    p = info["tags"]
-    tag = []
-    for i in range(len(p)):
-        tag.append((p[i]['tag'])[1])
-    return tag
-
-def get_img_url():
-    p = info["files"]
-    img_url = []
-    for i in range(len(p)):
-        img_url.append((p[i]['image']['url']))
-    return img_url
-
-info = fetch_gallery_info_http(gallery_id)
-
-def download_one(args):
-    def_url, folder, idx, ext = args
-    path = os.path.join(folder, f"{folder}_{idx}.{ext}")
-    resp = requests.get(def_url, stream=True, timeout=10)
-    resp.raise_for_status()
-    with open(path, "wb") as f:
-        for chunk in resp.iter_content(8192):
-            f.write(chunk)
-    return path
-
-def download_images_parallel(urls, folder, def_n, def_m, def_workers=8, img_type=None):
-    os.makedirs(folder, exist_ok=True)
-    target_urls = urls[def_n - 1:def_m]
-    total = len(target_urls)
-    print(f" TO {folder} FOLDER, DOWNLOAD {total} IMAGE (TCP THREADS={def_workers})")
-
-    tasks = []
-    with ThreadPoolExecutor(max_workers=def_workers) as exe:
-        for idx, def_url in enumerate(target_urls, start=def_n):
-            if img_type:
-                ext = img_type
-            else:
-                ext = def_url.split("?")[0].split(".")[-1]
-            tasks.append(exe.submit(download_one, (def_url, folder, idx, ext)))
-
-        for future in as_completed(tasks):
-            print("SAVED BY:", future.result())
-    print("DONE")
-
-print("-------------------------")
-print("FILE INFO")
-print("-------------------------")
-print(f'URL: ｢{str(url)}｣')
-print(f'GALLERY ID: ｢{gallery_id}｣')
-print(f'TITLE: ｢{info["title"]}｣')
-print(f'PAGES: ｢{info["filecount"]}｣')
-print("-------------------------")
-print(f'TAGS: ｢{", ".join(get_tags())}｣')
-print("-------------------------")
-print("BEFORE DOWNLOAD . . . ")
-print("-------------------------")
-
-n = int(input('page from: '))
-m = int(input('page to: '))
-if m > info["filecount"]:
-    m = info["filecount"]
-    print("page to can't exceed the total page")
-    print("page to set to end of gallery")
-
-ext_type = input('file extension (e.g. jpg, webp): ')
-
-workers = os.cpu_count() * 5
-print(f'TCP THREADS: ｢{workers}｣')
-start_time = time.time()
-download_images_parallel(get_img_url(), str(gallery_id), n, m, workers, img_type=ext_type)
-end_time = time.time()
-print(f"DOWNLOAD COMPLETE IN {end_time - start_time:.2f} SECONDS")
 
 
 
