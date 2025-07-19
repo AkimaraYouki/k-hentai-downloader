81fb7302405f1f5509ae3be0f8387899459488ba
parksuho <tngh860@naver.com>
2025-07-19 17:18:15 +0900

    add some info to README.md

---
+++ doodle.py
@@ -0,0 +1,18 @@
+
+# K-hentai . org 의 북마크 익스포트 형식
+# bookmark = [{"name":"ㄷㄱㅈㄷㄱ","private":1,"created":1752910021964,"updated":1752910046812,"galleries":[{"id":3446484,"time":1752910035865},{"id":3446480,"time":1752910046812}]}]
+# print(bookmark[0]["galleries"])
+#
+# list = []
+# for i in bookmark[0]["galleries"]:
+#     print(i["id"])
+#     list.append(i["id"])
+# print(','.join(map(str, list)))
+
+
+# list_1 = []
+# for i in download.split("\n"):
+#     list_1.append(i.split(" ")[0])
+# print(",".join(list_1))
+
+
